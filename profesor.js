e345a1775959f918490f98de2d51daad37e9b587
Ireara <ica36@alu.ua.es>
2019-04-19 12:38:55 +0200

    [ Profesor ] Terminado
    Página del profesor totalmente terminada mostrando todos los resultados del alumno

--- profesor.js
+++ profesor.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 var aciertosplaya=erroresplaya=nojuegaplaya=0;
 var aciertosmanzana=errorescontar=erroresrestar=0;
-
+var aciertosMayor=erroresMayor=errorMonedas=aciertosMonedas=muchasMonedas=0;
 /*      --Playa--       */
 if(sessionStorage.getItem("pAciertos")!=null){
    aciertosplaya = JSON.parse(sessionStorage.pAciertos);
@@ -28,22 +28,43 @@ if(sessionStorage.getItem("mErroresM")!=null){
 }
 /*      --Monedas--     */
 
-var aciertosTOT = aciertosplaya+ aciertosmanzana//+aciertos que falta;
-var erroresTOT = erroresplaya+errorescontar+erroresrestar;
+if(sessionStorage.getItem("tMayorCorrecto")!=null){
+   aciertosMayor = JSON.parse(sessionStorage.tMayorCorrecto);
+}
+ 
+if(sessionStorage.getItem("tMayorIncorrecto")!=null){
+   erroresMayor = JSON.parse(sessionStorage.tMayorIncorrecto);
+}
+  
+if(sessionStorage.getItem("tCorrecto")!=null){
+   aciertosMonedas =  JSON.parse(sessionStorage.tCorrecto);
+}
+
+if(sessionStorage.getItem("tMonedasmal")!=null){
+   errorMonedas =  JSON.parse(sessionStorage.tMonedasmal);
+}
+
+if(sessionStorage.getItem("tNominimo")!=null){
+   muchasMonedas =  JSON.parse(sessionStorage.tCorrecto);
+}
+
+$("#monedas").append("<p>Sabe cual numero es mayor: "+aciertosMayor+"</p>");
+$("#monedas").append("<p>No sabe cual numero es mayor: "+erroresMayor+"</p>");
+$("#monedas").append("<p>Usa el mínmo nº de monedas: "+aciertosMonedas+"</p>");
+$("#monedas").append("<p>Usa monedas de más: "+muchasMonedas+"</p>");
+$("#monedas").append("<p>No ha alcanzado el precio: "+errorMonedas+"</p>");
 
 
-$("#general").append("<p>Aciertos Totales: "+aciertosTOT+"</p>");
-$("#general").append("<p>Errores Totales: "+erroresTOT+"</p>");
+
+
 
 $("#playa").append("<p>Sin responder: "+nojuegaplaya+"</p>");
 $("#playa").append("<p>Errores: "+erroresplaya+"</p>");
 $("#playa").append("<p>Aciertos: "+aciertosplaya+"</p>");
 
-//$("#manzanas").append("<p>Errores al contar: "+errorescontar+"</p>");
+$("#manzanas").append("<p>Errores al quitar: "+errorescontar+"</p>");
 $("#manzanas").append("<p>Errores al restar: "+erroresrestar+"</p>");
 $("#manzanas").append("<p>Aciertos: "+aciertosmanzana+"</p>");
 
-$("#monedas").append("<p>Errores al comprar: "+errorescontar+"</p>");
-$("#monedas").append("<p>Errores al contar: "+erroresrestar+"</p>");
-$("#monedas").append("<p>Aciertos: "+aciertosmanzana+"</p>");
+
 })
